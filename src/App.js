274ea6a86778c9d3ca382353d33e2aba38f4d12f
Luis Javier López Arredondo <ljDevelopment.6.28@gmail.com>
2021-01-26 00:06:39 +0100

    Following Kata1 (redux)... added random TODO

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {connect} from 'react-redux';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <pre>
+			<button onClick={() => props.addRandomTodo()}>
+				add todo
+			</button>
+			{
+				JSON.stringify(props, null, 2)
+			}
+		</pre>
     </div>
   );
 }
 
-export default App;
+const mapStateToProps = state => ({state : state}) 
+const mapDispatchToProps = (dispacth) => ({
+
+	addRandomTodo: () => dispacth({
+		type: 'ADD_TODO',
+		payload: 'borra esta tarea'
+	})
+})
+const connectAccp = connect(
+	mapStateToProps, 
+	mapDispatchToProps)(App)
+
+export default connectAccp;
